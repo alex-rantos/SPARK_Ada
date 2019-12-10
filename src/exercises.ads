a5e7184f56e250a0d3d3ee3a51e5ed7c049d550c
Alexandros Rantos <a.rantos@hotmail.com>
2019-12-10 01:19:39 +0000

    information flow added

--- src/exercises.ads
+++ src/exercises.ads
@@ -3,13 +3,36 @@ package exercises with SPARK_Mode is
    subtype Size is Integer range 0..20;
    type SmallArray is array (Size) of Integer;
 
+   Cost: Integer;
+   Result: Integer;
 
-   procedure Calc(Cost: in Integer; Result: out Integer);
+   procedure Calc(Cost: in Integer; Result: out Integer)
+     with
+       Global => (Input => Cost,
+                 Output => Result),
+       Depends => (Result => Charge)
+       Pre => (Cost < Integer'Last),
+       Post => (X < Integer'Last, X >= Cost'Old);
 
-   procedure CalculateCharge(Quantity: in Integer; Charge: out Integer);
+   procedure CalculateCharge(Quantity: in Integer; Charge: out Integer)
+     with
+       Global => (Input => Quantity,
+                 Output => Charge),
+     Depends => (Charge => Quantity)
+       Pre => (Quantity < Integer'Last),
+       Post => (Charge < Integer'Last, Charge >= Cost'Old);
 
-   procedure Modify(A: in out SmallArray; B, C: in Integer);
 
-   procedure MaskSequence (Target: in out SmallArray; Mask: in SmallArray);
+   procedure Modify(A: in out SmallArray; B, C: in Integer)
+     with
+       Global => (In_out => A, Input => (B,C)),
+       Depends => (A => (B,C)),
+       Pre => (B < Size'Max, C < Size'Max, B + C < Size'Max));
+
+   procedure MaskSequence (Target: in out SmallArray; Mask: in SmallArray)
+     with
+       Global => (in_out => Target,
+                  input => Mask),
+       Depends => (Target => (T,Mask));
 
 end exercises;
